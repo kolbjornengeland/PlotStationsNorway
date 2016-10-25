eca3cc88c1a7db3c1b81e253197ded692c14d54f
kolbjornengeland <koe@nve.no>
2016-10-25 06:21:52 +0200

    Improvements

--- Plotte_kart.R
+++ Plotte_kart.R
@@ -1,16 +1,88 @@
 
 library('maptools')
 
+GisFolder<-'inst/GISDATA/'
+
+load_data_covers(GisFolder)
+
+
+load_data_covers<-function(GisFolder){
+
+  #Omriss av Norge
+  Norge<<-readShapeSpatial(paste(GisFolder,'norge.shp',sep=''))
+  
+  # Shape fil med grenser for nedbørsflet
+  Mycatchments<<-readShapeSpatial(paste(GisFolder,'Hydrologi_TotalNedborfeltMalestasjon.shp',sep=''))
+  
+  # Shape fil med målestasjoner
+  MyStations<<-readShapeSpatial(paste(GisFolder,'Hydrologi_MaleserieMalestasjon.shp',sep=''))
+  spar<<-read.table("inst/feltparametre_flomstasjoner145.txt",sep="\t",header=TRUE)
+  regine_nr<-as.integer(spar[,2]/100000000)
+  main_nr<-(spar[,2]-regine_nr*100000000)/1000
+  mnumbers<-(regine_nr*10000+main_nr)
+# Sort the list according to station number
+  spar<-spar[order(mnumbers),]
+                        
+# Select only streamflow stations
+  MyStations<-MyStations[MyStations$stParaKode==1001,]
+                        
+  #Select only unique versions (some are duplicated)
+  MyStations<-MyStations[!(duplicated(MyStations$stSamletID)),]
+                                      
+  forecasting_c<<-Mycatchments[which((as.integer(substr(Mycatchments$stSamletID,1,4))*10000+as.integer(substr(Mycatchments$stSamletID,5,9)))%in%mnumbers),]
+  forecasting_c<<-forecasting_c[order(forecasting_c$stSamletID),]                        
+  forecasting_s<<-MyStations[which((as.integer(substr(MyStations$stSamletID,1,4))*10000+as.integer(substr(MyStations$stSamletID,5,9)))%in%mnumbers),]
+  forecasting_s<<-forecasting_s[order(forecasting_s$stSamletID),]
+}
+
+
+cvalues<-corr_all
+vindex<-1
+c_index<-10
+cbins<-c(-1.0,0.2,0.3,0.4,0.5,0.6,0.7,0.8)
+
+
+
+plot_map_points<-function(cvalues,v_index=NA,c_index=NA,p_value=NA,s_value=NA,cbins<-c(-1.0,0.2,0.3,0.4,0.5,0.6,0.7)){
+windows(7,8)
+par(mar=c(0,0,1,0))
+plot(Norge)
+
+which(forecasting_s$stID==paste(rownames(corr_all),'.0',sep=''))
+forecasting_ss<-forecasting_s[match(paste(rownames(corr_all),'.0',sep=''),forecasting_s$stID)]
+
+nco<-length(cbins)-1
+farve = brewer.pal(nco, "Spectral")
+
+loc_tmp1 <- which(cvalues[,v_index] <= cvalues[,c_index])
+
+points(forecasting_eval[loc_tmp1,],pch=19,cex=1.5, col="grey)
+
+
+
+loc_tmp2 <- which(corr_januar > rr & corr_januar <= 0.3)
+
+points(forecasting_eval[loc_tmp2,],pch=19,cex=1.5, col=farve[1])
+
+loc_tmp3 <- which(corr_januar > 0.3 & corr_januar <= 0.4)
+
+points(forecasting_eval[loc_tmp3,],pch=19,cex=1.5, col=farve[2])
+
+
+
+
+
+
+}
+
+
+
+
+
 setwd('//nve/fil/h/HM/Interne Prosjekter/ensembleprognoser/script')
 
-spar<-read.table("../GISData/feltparametre_flomstasjoner145.txt",sep="\t",header=TRUE)
-regine_nr<-as.integer(spar[,2]/100000000)
-main_nr<-(spar[,2]-regine_nr*100000000)/1000
 
 
-mnumbers<-(regine_nr*10000+main_nr)
-# Sort the list according to station number
-spar<-spar[order(mnumbers),]
 
 #Histogram av middelh�yde
 hist(spar[,18],xlab="Median h�yde",ylab="Antall",main="")
@@ -38,36 +110,8 @@ for(i in 1 : ns){
 }
 
 
-#Omriss av Norge
-Norge=readShapeSpatial('../GISDATA/norge.shp')
-
-# Shape fil med grenser for nedbørsflet
-Mycatchments=readShapeSpatial('../GISDATA/Hydrologi_TotalNedborfeltMalestasjon.shp')
 
 
-# Shape fil med målestasjoner
-MyStations=readShapeSpatial('../GISDATA/Hydrologi_MaleserieMalestasjon.shp')
-
-
-
-
-# Select only streamflow stations
-MyStations<-MyStations[MyStations$stParaKode==1001,]
-
-#Select only unique versions (some are duplicated)
-MyStations<-MyStations[!(duplicated(MyStations$stSamletID)),]
-
-
-#LESER stasjonsliste
-#stationlist<-read.table('lavvannsdata.txt',skip=1)
-
-# 
-#substr(Mycatchments$stSamletID[1],1,4)
-
-
-regine_nr<-as.integer(spar[,2]/100000000)
-main_nr<-(spar[,2]-regine_nr*100000000)/1000
-mnumbers<-(regine_nr*10000+main_nr)
 #Need to use this since the GIS data has used station number ##162.4 in stead of 162.8
 #mnumbers[135]=162*10000+4
 
@@ -86,27 +130,3 @@ y_max<-max(summary(Norge)$bbox[4])
 
 
 # Plotter kart
-windows(7,8)
-par(mar=c(0,0,0,0))
-plot(Norge)
-plot(forecasting_c,col="grey",add=TRUE)
-#plot(forecasting_s,col=7,add=TRUE)
-#plot(lavvannsstasjoner,col=2,add=TRUE)
-points(forecasting_s,pch=19,cex=1.0,col="lightblue")
-ns=length(lavvannsstasjoner)
-
-# fargekoding ekempel
-rbPal <- colorRampPalette(c('blue','red'))
-
-#Lager 10 fargeklasser
-#Gir hver stasjonspunkt en tilfeldig farge. Kan bruke en annen #fargeinndeling.
-
-pcol <- rbPal(10)[as.numeric(cut(ave_temp[,13],breaks = c(-4,-3,-2,-1,0,1,2,3,4,5,6)))]
-
-
-points(forecasting_s,pch=19,cex=1.0,col=pcol)
-
-
-
-
-
